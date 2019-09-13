bfc9e5cf99c9df0a05206daae9a1ffc92eec01d1
fairywellos <fairybutterfly73@gmail.com>
2019-09-13 12:50:50 -0400

    remove external js

--- tradelist-app/nuxt.config.js
+++ tradelist-app/nuxt.config.js
@@ -22,6 +22,11 @@ module.exports = {
         ],
         link: [
             {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+        ],
+        script: [
+            {
+                src: "js/main.js"
+            }
         ]
     },
 
