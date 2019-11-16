3a78cf36084bd9dde0e415d56b1dcdf9b883d6b3
fairywellos <fairybutterfly73@gmail.com>
2019-11-16 10:26:11 -0800

    ads

--- tradelist-app/pages/listing-details/_id.vue
+++ tradelist-app/pages/listing-details/_id.vue
@@ -3,9 +3,21 @@
 		:class="isAuthenticated &&  listing.user.id !== loggedInUser.id ? '' : 'listing_owner'"
 		class="product_page"
 	>
+
 		<div class="container">
 			<Header v-if="$device.isDesktop"/>
 			<HeaderMobile v-if="$device.isMobileOrTablet"/>
+			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<!-- Listing Detail -->
+	<ins class="adsbygoogle"
+	style="display:block"
+	data-ad-client="ca-pub-1709497292936218"
+	data-ad-slot="4464166452"
+	data-ad-format="auto"
+	data-full-width-responsive="true"></ins>
+	<script>
+	(adsbygoogle = window.adsbygoogle || []).push({});
+	</script>
 			<no-ssr>
 				<modal v-if="showModalMessage" class="modal_message" @close="closeModalMessage()">
 					<div slot="body">
@@ -475,17 +487,7 @@
 			</modal>
 		</no-ssr>
 	</div>
-	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-	<!-- Listing Detail -->
-	<ins class="adsbygoogle"
-	style="display:block"
-	data-ad-client="ca-pub-1709497292936218"
-	data-ad-slot="4464166452"
-	data-ad-format="auto"
-	data-full-width-responsive="true"></ins>
-	<script>
-	(adsbygoogle = window.adsbygoogle || []).push({});
-	</script>
+	
 </template>
 
 <script>
